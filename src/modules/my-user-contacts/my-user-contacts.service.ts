2c6bc025832f575ca88e905c68b4b10b519c156f
jimmymaise <the@duyet.com>
2021-02-02 09:49:12 +0700

    fix my contact

--- src/modules/my-user-contacts/my-user-contacts.service.ts
+++ src/modules/my-user-contacts/my-user-contacts.service.ts
@@ -1,75 +1,77 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common';
 
-import { PrismaService } from '../prisma/prisma.service'
-import {
-  MyUserContact,
-} from '@prisma/client'
-import { PaginationDTO } from '../../models'
+import { PrismaService } from '../prisma/prisma.service';
+import { MyUserContact } from '@prisma/client';
+import { PaginationDTO } from '../../models';
 
 @Injectable()
 export class MyUserContactsService {
-  constructor(
-    private prismaService: PrismaService
-  ) {}
+  constructor(private prismaService: PrismaService) {}
 
-  addUserToMyContactList(myUserId: string, connectToUserId: string): Promise<MyUserContact> {
+  addUserToMyContactList(
+    myUserId: string,
+    connectToUserId: string,
+  ): Promise<MyUserContact> {
     return this.prismaService.myUserContact.create({
       data: {
         ownerUserId: myUserId,
         user: {
           connect: {
-            id: connectToUserId
-          }
-        }
-      }
-    })
+            id: connectToUserId,
+          },
+        },
+      },
+    });
   }
 
-  async deleteUserFromMyContactList(myUserId: string, removeUserIdFromMyList: string): Promise<MyUserContact> {
+  async deleteUserFromMyContactList(
+    myUserId: string,
+    removeUserIdFromMyList: string,
+  ): Promise<MyUserContact> {
     const item = await this.prismaService.myUserContact.findUnique({
       where: {
         ownerUserId_userId: {
           ownerUserId: myUserId,
-          userId: removeUserIdFromMyList
-        }
-      }
-    })
+          userId: removeUserIdFromMyList,
+        },
+      },
+    });
 
     if (!item) {
-      throw new Error('This contact not exist')
+      throw new Error('This contact not exist');
     }
 
     return this.prismaService.myUserContact.delete({
       where: {
         ownerUserId_userId: {
           ownerUserId: myUserId,
-          userId: removeUserIdFromMyList
-        }
-      }
-    })
+          userId: removeUserIdFromMyList,
+        },
+      },
+    });
   }
 
   async findAllMyContactList({
     userId,
     offset = 0,
-    limit = 10
+    limit = 10,
   }): Promise<PaginationDTO<MyUserContact>> {
     const where = {
-      ownerUserId: userId
-    }
+      ownerUserId: userId,
+    };
 
     const items = await this.prismaService.myUserContact.findMany({
       where,
     });
-    const count = await this.prismaService.item.count({
-      where
+    const count = await this.prismaService.myUserContact.count({
+      where,
     });
 
     return {
       items,
       total: count,
       offset,
-      limit
+      limit,
     };
   }
 }
