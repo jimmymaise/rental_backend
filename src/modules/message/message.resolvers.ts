7bc17b050cc30deb197faa163d3ed35cd8ed8123
jimmymaise <the@duyet.com>
2021-02-09 19:08:39 +0700

    update

--- src/modules/message/message.resolvers.ts
+++ src/modules/message/message.resolvers.ts
@@ -1,22 +1,19 @@
-import { UseGuards } from '@nestjs/common'
-import { Args, Resolver, Mutation, Query } from '@nestjs/graphql'
-import { ChatConversation } from '@prisma/client';
+import { UseGuards } from '@nestjs/common';
+import { Args, Resolver, Mutation, Query } from '@nestjs/graphql';
 
-import { MessageService } from './message.service'
-import {
-  ChatConversationDTO
-} from './chat-conversation.dto';
-import { PaginationDTO } from '../../models'
-import { GqlAuthGuard } from '../auth/gpl-auth.guard'
-import { CurrentUser } from '../auth/current-user.decorator'
-import { GuardUserPayload } from '../auth/auth.dto'
-import { UsersService } from '../users/users.service'
+import { MessageService, MessageInfoModel } from './message.service';
+import { ChatConversationDTO } from './chat-conversation.dto';
+import { PaginationDTO } from '../../models';
+import { GqlAuthGuard } from '../auth/gpl-auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { GuardUserPayload } from '../auth/auth.dto';
+import { UsersService } from '../users/users.service';
 
 @Resolver('Message')
 export class MessageResolvers {
   constructor(
     private messageService: MessageService,
-    private userService: UsersService
+    private userService: UsersService,
   ) {}
 
   @Mutation()
@@ -25,100 +22,123 @@ export class MessageResolvers {
     @CurrentUser() user: GuardUserPayload,
     @Args('chatWithUserId') chatWithUserId: string,
   ): Promise<ChatConversationDTO> {
-    const members = [user.id, chatWithUserId]
-    let existingSession = await this.messageService.findConversationUniqueByMembers(members)
+    const members = [user.id, chatWithUserId];
+    let existingSession = await this.messageService.findConversationUniqueByMembers(
+      members,
+    );
 
     if (!existingSession) {
-      existingSession = await this.messageService.createNewConversation(members, user.id)
+      existingSession = await this.messageService.createNewConversation(
+        members,
+        user.id,
+      );
     }
 
-    const memberDetails = []
+    const memberDetails = [];
     for (let i = 0; i < members.length; i++) {
-      const userInfo = await this.userService.getUserDetailData(members[i])
+      const userInfo = await this.userService.getUserDetailData(members[i]);
       memberDetails.push({
         id: userInfo.id,
         displayName: userInfo.displayName,
         avatarImage: userInfo.avatarImage,
-        coverImage: userInfo.coverImage
-      })
+        coverImage: userInfo.coverImage,
+      });
     }
 
     return {
       id: existingSession.id,
-      members: memberDetails
-    }
+      members: memberDetails,
+    };
   }
 
   @Query()
   @UseGuards(GqlAuthGuard)
   async feedMyConversations(
     @CurrentUser() user: GuardUserPayload,
-    @Args('query') query: {
-      offset: number,
-      limit: number
-    }
+    @Args('query')
+    query: {
+      offset: number;
+      limit: number;
+    },
   ): Promise<PaginationDTO<ChatConversationDTO>> {
-    const { offset, limit } = query || {}
-    const actualLimit = limit && limit > 100 ? 100 : limit
+    const { offset, limit } = query || {};
+    const actualLimit = limit && limit > 100 ? 100 : limit;
 
     const result = await this.messageService.findAllMyConversations({
       offset,
       limit,
-      userId: user.id
-    })
-    const items = []
+      userId: user.id,
+    });
+    const items = [];
 
     for (let i = 0; i < result.items.length; i++) {
-      const currentItem: any = result.items[i]
+      const currentItem: any = result.items[i];
 
-      const onlyVisibleWhenFromUserOrHaveMessageToAvoidSpamConversation = currentItem.createdBy === user.id || currentItem.chatMessages.length
+      const onlyVisibleWhenFromUserOrHaveMessageToAvoidSpamConversation =
+        currentItem.createdBy === user.id || currentItem.chatMessages.length;
       const newItem = {
         id: currentItem.id,
         lastMessages: [],
         members: [],
-        isVisible: onlyVisibleWhenFromUserOrHaveMessageToAvoidSpamConversation
-      }
+        isVisible: onlyVisibleWhenFromUserOrHaveMessageToAvoidSpamConversation,
+      };
 
-      const lastMessages = []
+      const lastMessages = [];
       for (let j = 0; j < currentItem.chatMessages.length; j++) {
-        const message = currentItem.chatMessages[j]
+        const message = currentItem.chatMessages[j];
         lastMessages.push({
           id: message.id,
           content: message.content,
           replyToId: message.replyToId,
           chatConversationId: message.chatConversationId,
           fromUserId: message.fromUserId,
-          fromUserInfo: await this.userService.getUserDetailData(message.fromUserId),
+          fromUserInfo: await this.userService.getUserDetailData(
+            message.fromUserId,
+          ),
           isRead: message.isRead,
-          createdDate: message.createdDate.getTime()
-        })
+          createdDate: message.createdDate.getTime(),
+        });
       }
-      newItem.lastMessages = lastMessages
+      newItem.lastMessages = lastMessages;
 
-      const memberDetails = []
-      const conversationMembers = currentItem['chatConversationMembers']
+      const memberDetails = [];
+      const conversationMembers = currentItem['chatConversationMembers'];
       for (let i = 0; i < conversationMembers.length; i++) {
-        const userInfo = await this.userService.getUserDetailData(conversationMembers[i].userId)
+        const userInfo = await this.userService.getUserDetailData(
+          conversationMembers[i].userId,
+        );
         memberDetails.push({
           id: userInfo.id,
           displayName: userInfo.displayName,
           avatarImage: userInfo.avatarImage,
-          coverImage: userInfo.coverImage
-        })
+          coverImage: userInfo.coverImage,
+        });
       }
 
-      newItem.members = memberDetails
+      newItem.members = memberDetails;
 
-      items.push(newItem)
+      items.push(newItem);
     }
 
     return {
       items,
       total: result.total,
       offset: offset || 0,
-      limit: actualLimit
-    }
+      limit: actualLimit,
+    };
   }
 
-}
+  @Query()
+  @UseGuards(GqlAuthGuard)
+  async feedChatMessageInfo(
+    @CurrentUser() user: GuardUserPayload,
+  ): Promise<MessageInfoModel> {
+    const unReadCount = await this.messageService.countUnReadConversation(
+      user.id,
+    );
 
+    return {
+      unReadCount,
+    };
+  }
+}
--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -1,88 +1,127 @@
 import { Injectable } from '@nestjs/common';
-import { ChatConversation, ChatConversationMember, ChatMessage } from '@prisma/client';
+import {
+  ChatConversation,
+  ChatConversationMember,
+  ChatMessage,
+} from '@prisma/client';
 
 import { PrismaService } from '../prisma/prisma.service';
 import { PaginationDTO } from '../../models';
-import { UserInfoDTO } from '../users/user-info.dto'
-import { UsersService } from '../users/users.service'
+import { UserInfoDTO } from '../users/user-info.dto';
+import { UsersService } from '../users/users.service';
 
 interface MessageData {
-  id?: string
-  fromUserId: string
-  fromUserInfo?: UserInfoDTO
-  replyToId?: string
-  chatConversationId: string
-  content: string
+  id?: string;
+  fromUserId: string;
+  fromUserInfo?: UserInfoDTO;
+  replyToId?: string;
+  chatConversationId: string;
+  content: string;
+}
+
+export interface MessageInfoModel {
+  unReadCount: number;
 }
 
 @Injectable()
 export class MessageService {
   constructor(
     private prismaService: PrismaService,
-    private userService: UsersService
+    private userService: UsersService,
   ) {}
 
-  createNewConversation(memberIds: string[], createdBy: string): Promise<ChatConversation> {
+  createNewConversation(
+    memberIds: string[],
+    createdBy: string,
+  ): Promise<ChatConversation> {
     return this.prismaService.chatConversation.create({
       data: {
         createdBy,
         chatConversationMembers: {
           create: memberIds.map((item) => ({
-            userId: item
-          }))
-        }
-      }
-    })
+            userId: item,
+          })),
+        },
+      },
+    });
   }
 
-  async isUserMemberInTheConversation(conversationId: string, memberId: string): Promise<boolean> {
-    return await this.prismaService.chatConversationMember.findUnique({
-      where: {
-        chatConversationId_userId: {
-          chatConversationId: conversationId,
-          userId: memberId
-        }
-      }
-    }) !== null
+  async isUserMemberInTheConversation(
+    conversationId: string,
+    memberId: string,
+  ): Promise<boolean> {
+    return (
+      (await this.prismaService.chatConversationMember.findUnique({
+        where: {
+          chatConversationId_userId: {
+            chatConversationId: conversationId,
+            userId: memberId,
+          },
+        },
+      })) !== null
+    );
   }
 
-  findConversationUniqueByMembers(memberIds: string[]): Promise<ChatConversation> {
+  findConversationUniqueByMembers(
+    memberIds: string[],
+  ): Promise<ChatConversation> {
     const orConditional = memberIds.map((userId) => ({
-      userId
-    }))
+      userId,
+    }));
     return this.prismaService.chatConversation.findFirst({
       where: {
         chatConversationMembers: {
           every: {
-            OR: orConditional
-          }
+            OR: orConditional,
+          },
         },
       },
       include: {
         chatConversationMembers: true,
-      }
-    })
+      },
+    });
   }
 
   getConversation(id: string): Promise<ChatConversation> {
-    return this.prismaService.chatConversation.findUnique({ where: { id } })
+    return this.prismaService.chatConversation.findUnique({ where: { id } });
   }
 
   getConversationMembers(id: string): Promise<ChatConversationMember[]> {
-    return this.prismaService.chatConversation.findUnique({ where: { id } }).chatConversationMembers({})
+    return this.prismaService.chatConversation
+      .findUnique({ where: { id } })
+      .chatConversationMembers({});
+  }
+
+  async countUnReadConversation(userId: string): Promise<number> {
+    const unReadCount = await this.prismaService.chatConversation.count({
+      where: {
+        chatConversationMembers: {
+          some: {
+            userId,
+          },
+        },
+        chatMessages: {
+          some: {
+            isRead: false,
+          },
+        },
+      },
+    });
+
+    return unReadCount;
   }
 
   async findAllMyConversations({
     offset = 0,
     limit = 10,
-    userId
+    userId,
   }): Promise<PaginationDTO<ChatConversation>> {
     const items = await this.prismaService.chatConversation.findMany({
       where: {
         chatConversationMembers: {
           some: {
-            userId
-          }
+            userId,
+          },
         },
       },
       include: {
@@ -90,58 +129,63 @@ export class MessageService {
         chatMessages: {
           where: {
             replyBy: {
-              is: null
-            }
-          }
-        }
+              is: null,
+            },
+          },
+        },
       },
       skip: offset,
-      take: limit
+      take: limit,
     });
     const count = await this.prismaService.chatConversation.count({
       where: {
         chatConversationMembers: {
           some: {
-            userId
-          }
-        }
-      }
+            userId,
+          },
+        },
+      },
     });
 
     return {
       items,
       total: count,
       offset,
-      limit
+      limit,
     };
   }
 
-  addMessage({ fromUserId, replyToId, chatConversationId, content }: MessageData): Promise<ChatMessage> {
+  addMessage({
+    fromUserId,
+    replyToId,
+    chatConversationId,
+    content,
+  }: MessageData): Promise<ChatMessage> {
     const data: any = {
       fromUserId,
       content,
       chatConversation: {
         connect: {
-          id: chatConversationId
-        }
-      }
-    }
+          id: chatConversationId,
+        },
+      },
+    };
 
     if (replyToId) {
       data.replyTo = {
         connect: {
-          id: replyToId
-        }
-      }
+          id: replyToId,
+        },
+      };
     }
 
     return this.prismaService.chatMessage.create({
-      data
-    })
+      data,
+    });
   }
 
   async findAllMyMessagesInConversation({
-    conversationId
+    conversationId,
   }): Promise<MessageData[]> {
     // TODO: HARD CODE 500 Messages
     const items = await this.prismaService.$queryRaw(
@@ -160,13 +204,15 @@ export class MessageService {
         SELECT * FROM recursive_message_query
         ORDER BY root desc
         LIMIT 500
-      `
-    )
+      `,
+    );
 
-    const result: MessageData[] = []
+    const result: MessageData[] = [];
     for (let i = 0; i < items.length; i++) {
-      const currentItem = items[i]
-      const fromUserInfo = await this.userService.getUserDetailData(currentItem['fromuserid'])
+      const currentItem = items[i];
+      const fromUserInfo = await this.userService.getUserDetailData(
+        currentItem['fromuserid'],
+      );
 
       result.push({
         id: currentItem['id'],
@@ -175,14 +221,14 @@ export class MessageService {
           id: fromUserInfo.id,
           displayName: fromUserInfo.displayName,
           avatarImage: fromUserInfo.avatarImage,
-          coverImage: fromUserInfo.coverImage
+          coverImage: fromUserInfo.coverImage,
         },
         replyToId: currentItem['replytoid'],
         chatConversationId: currentItem['chatconversationid'],
-        content: currentItem['content']
-      })
+        content: currentItem['content'],
+      });
     }
 
-    return result
+    return result;
   }
 }
