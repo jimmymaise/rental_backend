c764842f8ff812267259acf8c6917ca611d59eef
jimmymaise <the@duyet.com>
2021-05-03 12:01:08 +0700

    add permission for areas

--- src/modules/areas/areas.resolvers.ts
+++ src/modules/areas/areas.resolvers.ts
@@ -2,12 +2,15 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 
 import { AreasService } from './areas.service';
 import { Area } from '@prisma/client';
+import { Permission } from '@modules/auth/permission/permission.enum';
+import { Permissions } from '@modules/auth/permission/permissions.decorator';
 
 @Resolver('Area')
 export class AreasResolvers {
   constructor(private readonly areasService: AreasService) {}
 
   @Query()
+  @Permissions(Permission.NO_NEED_LOGIN)
   async getAllAvailableAreas(
     @Args('isDisabled')
     isDisabled: boolean,
