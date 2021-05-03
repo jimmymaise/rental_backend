e95ed31d1769cc5b8b9e91f3a3e71809e4640e46
jimmymaise <the@duyet.com>
2021-05-03 22:21:38 +0700

    merge migrate

--- prisma/migrations/20210502143706_migrate_item_org/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Item" ALTER COLUMN "orgId" DROP NOT NULL;
--- prisma/migrations/20210503135441_migrateuseritem/migration.sql
+++ prisma/migrations/20210503135441_migrateuseritem/migration.sql
@@ -1,12 +1,24 @@
 /*
   Warnings:
 
+  - The `images` column on the `Item` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `checkBeforeRentDocuments` column on the `Item` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `keepWhileRentingDocuments` column on the `Item` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `files` column on the `RentingItemRequestActivity` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `avatarImage` column on the `UserInfo` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `coverImage` column on the `UserInfo` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - The `data` column on the `UserNotification` table would be dropped and recreated. This will lead to data loss if there is data in the column.
 
 */
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN     "orgId" TEXT,
+DROP COLUMN "images",
+ADD COLUMN     "images" JSONB,
+DROP COLUMN "checkBeforeRentDocuments",
+ADD COLUMN     "checkBeforeRentDocuments" JSONB,
+DROP COLUMN "keepWhileRentingDocuments",
+ADD COLUMN     "keepWhileRentingDocuments" JSONB;
+
 -- AlterTable
 ALTER TABLE "RentingItemRequestActivity" DROP COLUMN "files",
 ADD COLUMN     "files" JSONB;
@@ -20,3 +32,6 @@ ADD COLUMN     "coverImage" JSONB;
 -- AlterTable
 ALTER TABLE "UserNotification" DROP COLUMN "data",
 ADD COLUMN     "data" JSONB;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD FOREIGN KEY ("orgId") REFERENCES "Organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
