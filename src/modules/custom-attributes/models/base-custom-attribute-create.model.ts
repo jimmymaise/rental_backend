456fd45880ac3a4ab3ce3508d1a68e1424351358
jimmymaise <the@duyet.com>
2021-06-01 20:46:04 +0700

    update data initilize

--- src/modules/custom-attributes/models/base-custom-attribute-create.model.ts
+++ src/modules/custom-attributes/models/base-custom-attribute-create.model.ts
@@ -4,6 +4,7 @@ export class BaseCustomAttributeCreateModel {
   public value: string;
   public label: string;
   public description?: string;
+  public parentAttributeValue?: string;
   public isDefault?: boolean;
   public isDisabled?: boolean;
   public order?: number;
@@ -19,6 +20,7 @@ export class BaseCustomAttributeCreateModel {
       label: data.label,
       isDefault: false,
       isDisabled: data.isDisabled,
+      parentAttributeValue: data.parentAttributeValue,
       org: {
         connect: {
           id: orgId,
